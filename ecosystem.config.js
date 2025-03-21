413e1973dec68302367f54afbac27b0f3748564d
Niskarsh <niskarshk@gmail.com>
2025-03-21 23:18:09 +0530

    added ecosystemconfig

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+      {
+        name: 'ai-sales-tester',  // Name of the application
+        script: 'npm',   // The script to execute
+        args: 'start',   // The command arguments (npm start)
+        watch: false,    // Disable watching files for changes
+      },
+    ],
+  };
+  
